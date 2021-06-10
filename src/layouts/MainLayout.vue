16923b91793624e5f0c967eba4dcecc53842a3c1
Arif_Gafizov <agafizov@gmail.com>
2021-06-10 21:37:10 +0300

    Update MainLayout.vue

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,7 +3,7 @@
 
     <q-header elevated class="bg-primary text-white">
       <q-toolbar>
-        <q-btn dense flat round icon="menu" @click="left = !left" />
+        <q-btn dense flat round icon="menu" @click="left = !left"/>
         <q-toolbar-title>
           <q-avatar>
             <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
@@ -14,63 +14,10 @@
         <!--        auth form-->
 
         <div class="q-pa-md q-gutter-sm">
-          <q-btn  v-if="isAuth" label="Выйти" color="primary" @click="logout" />
-          <q-btn  v-else align="right" label="Войти" color="primary" @click="layout = true" />
+          <q-btn v-if="isAuth" label="Выйти" color="primary" @click="logout"/>
+          <q-btn v-else align="right" label="Войти" color="primary" @click="layout = true"/>
 
-          <q-dialog v-model="layout">
-            <q-layout view="Lhh lpR fff" container class="bg-white">
-              <q-header class="bg-primary">
-                <q-toolbar>
-                  <q-toolbar-title>Web store</q-toolbar-title>
-                  <q-btn flat v-close-popup round dense icon="close" />
-                </q-toolbar>
-              </q-header>
 
-              <q-footer class="bg-black text-white">
-                <q-toolbar inset>
-                  <q-toolbar-title>Web store</q-toolbar-title>
-                </q-toolbar>
-              </q-footer>
-
-              <q-page-container>
-                    <div class="q-pa-md" style="max-width: 400px">
-
-                      <q-form
-                        @submit="onSubmit"
-                        @reset="onReset"
-                        class="q-gutter-md"
-                      >
-                        <q-input
-                          filled
-                          v-model.trim="username"
-                          label="логин *"
-                          hint="Логин"
-                          lazy-rules
-                          :rules="[ val => val && val.length > 0 || 'Поле не может быть пустым']"
-                        />
-
-                        <q-input
-                          filled
-                          v-model.trim="password"
-                          label="пароль *"
-                          hint="Пароль"
-                          lazy-rules
-                          :rules="[ val => val && val.length > 0 || 'Поле не может быть пустым']"
-                        />
-
-<!--                        <q-toggle v-model="accept" label="I accept the license and terms" />-->
-
-                        <div>
-                          <q-btn @click.prevent="onSubmit" label="Отправить" type="submit" color="primary"/>
-                          <q-btn label="Сброс" type="reset" color="primary" flat class="q-ml-sm" />
-                        </div>
-                      </q-form>
-
-                    </div>
-
-              </q-page-container>
-            </q-layout>
-          </q-dialog>
         </div>
 
         <!--        auth form-->
@@ -79,26 +26,26 @@
 
     <q-drawer show-if-above v-model="left" side="left" bordered>
       <!-- drawer content -->
-                    <q-tabs
-                      v-model="tab"
-                      vertical
-                      class="text-teal"
-                    >
-                      <router-link :to="{ name: 'PRODUCTS_LIST'}">
-                        <q-tab name="products" icon="table_view" label="Товары" />
-                      </router-link>
-                      <router-link :to="{ name: 'ORDERS_LIST'}">
-                        <q-tab name="orders" icon="feed" label="Заказы" />
-                      </router-link>
-                      <router-link :to="{ name: 'CART'}">
-                        <q-tab name="cart" icon="shopping_cart" label="Корзина" />
-                      </router-link>
-                    </q-tabs>
+      <q-tabs
+        v-model="tab"
+        vertical
+        class="text-teal"
+      >
+        <router-link :to="{ name: 'PRODUCTS_LIST'}">
+          <q-tab name="products" icon="table_view" label="Товары"/>
+        </router-link>
+        <router-link :to="{ name: 'ORDERS_LIST'}">
+          <q-tab name="orders" icon="feed" label="Заказы"/>
+        </router-link>
+        <router-link :to="{ name: 'CART'}">
+          <q-tab name="cart" icon="shopping_cart" label="Корзина"/>
+        </router-link>
+      </q-tabs>
 
     </q-drawer>
 
     <q-page-container>
-      <router-view />
+      <router-view/>
     </q-page-container>
 
     <q-footer elevated class="bg-grey-8 text-white">
@@ -112,16 +59,122 @@
       </q-toolbar>
     </q-footer>
 
+    <q-dialog v-model="layout">
+
+      <q-card style="width: 700px; max-width: 80vw;">
+
+        <q-tabs
+          v-model="tab"
+          dense
+          class="text-grey"
+          active-color="primary"
+          indicator-color="primary"
+          align="justify"
+          narrow-indicator
+        >
+          <q-tab name="login" label="Log in" />
+          <q-tab name="signUp" label="Sign up" />
+          <q-btn icon="close" flat round dense v-close-popup />
+        </q-tabs>
+
+        <q-separator />
+
+        <q-tab-panels
+          v-model="tab"
+          animated
+          swipeable
+          vertical
+          transition-prev="jump-up"
+          transition-next="jump-up"
+        >
+
+          <q-tab-panel name="login">
+          <q-card-section>
+            <div class="text-h6">Введите свой логин и пароль</div>
+          </q-card-section>
+
+          <q-card-section>
+            <div class="q-pa-md" style="max-width: 800px">
+  <!--          <div class="q-pa-md" style="max-width: 800px">-->
+
+              <q-form
+                @submit="onSubmit"
+                @reset="onReset"
+                class="q-gutter-md"
+              >
+                <q-input
+                  filled
+                  v-model.trim="username"
+                  label="логин *"
+                  hint="Логин"
+                  lazy-rules
+                  :rules="[ val => val && val.length > 0 || 'Поле не может быть пустым']"
+                />
+
+                <q-input
+                  filled
+                  v-model.trim="password"
+                  label="пароль *"
+                  hint="Пароль"
+                  lazy-rules
+                  :rules="[ val => val && val.length > 0 || 'Поле не может быть пустым']"
+                />
+
+                <!--                        <q-toggle v-model="accept" label="I accept the license and terms" />-->
+                <br>
+                <q-separator />
+                <div  class="q-pa-md q-gutter-sm">
+                  <q-btn @click.prevent="onSubmit" label="Отправить" type="submit" color="primary"/>
+                  <q-btn label="Сброс" type="reset" color="primary" />
+                </div>
+              </q-form>
+
+            </div>
+
+          </q-card-section>
+          </q-tab-panel>
+
+          <q-tab-panel name="signUp">
+            <q-card-section>
+              <div class="text-h6">Зарегистрируйтесь</div>
+            </q-card-section>
+
+            <q-card-section>
+              <q-input
+                filled
+                v-model.trim="username"
+                label="логин *"
+                hint="Логин"
+                lazy-rules
+                :rules="[ val => val && val.length > 0 || 'Поле не может быть пустым']"
+              />
+
+              <q-input
+                filled
+                v-model.trim="password"
+                label="пароль *"
+                hint="Пароль"
+                lazy-rules
+                :rules="[ val => val && val.length > 0 || 'Поле не может быть пустым']"
+              />
+
+            </q-card-section>
+          </q-tab-panel>
+
+        </q-tab-panels>
+
+      </q-card>
+    </q-dialog>
   </q-layout>
 </template>
 
 <script>
-import { mapState } from 'vuex';
-import { mapMutations } from 'vuex';
+import {mapState} from 'vuex';
+import {mapMutations} from 'vuex';
 
 export default {
   // Store,
-  data () {
+  data() {
     return {
       layout: false,
       left: true,
@@ -137,7 +190,8 @@ export default {
   computed: {
     isAuth() {
       return this.$store.state.auth.isAuth;
-    }},
+    }
+  },
   // computed: mapState([
   //   // проксирует в this.isAuth доступ к store.state.isAuth
   //   'isAuth'
@@ -151,12 +205,12 @@ export default {
     //   'auth', // `this.auth()` будет вызывать `this.$store.commit('auth')`
     // //
     // ]),
-    onSubmit () {
+    onSubmit() {
 //          отправка пост запроса с данными username и password из формы
       this.$axios.post('/api/v1/users/auth/login/', {
         'username': this.username,
         'password': this.password,
-      }).then ((response) => {
+      }).then((response) => {
         console.log(response)
 //              сохранение в переменной токена авторизации полученного из response data
         const token = response.data.token
@@ -166,23 +220,22 @@ export default {
         this.layout = false
 
 
-      }).catch(function(error) {
+      }).catch(function (error) {
         console.log(error)
         alert(error)
       })
     },
 
-    onReset () {
+    onReset() {
       this.username = null
       this.password = null
       // this.accept = false
     },
-    logout () {
+    logout() {
       localStorage.removeItem('AUTH_TOKEN')
       this.$store.commit('auth/setAuth', false)
     }
   }
-
 
 
 }
