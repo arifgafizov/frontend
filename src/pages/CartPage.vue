8be86921600d409d7c7a1f070afd64abe5a2c12c
Arif_Gafizov <agafizov@gmail.com>
2021-06-11 20:09:36 +0300

    Update CartPage.vue

--- src/pages/CartPage.vue
+++ src/pages/CartPage.vue
@@ -1,12 +1,67 @@
 <template>
   <q-page>
-    Cart
+    <div class="text-h4">Корзина</div>
+
+    <template>
+      <div class="q-pa-md" style="max-width: 850px">
+        <q-list v-for="cart_product in cart_products" :key="cart_product.id" bordered separator>
+
+          <q-item clickable v-ripple>
+            <q-item-section>
+              <q-item-label>{{ cart_product.title }}</q-item-label>
+              <q-item-label caption>количество: {{ cart_product.quantity }},
+                цена: {{cart_product.price }} руб.</q-item-label>
+            </q-item-section>
+          </q-item>
+
+        </q-list>
+
+        <h5 class="text-h6">Общая стоимость товаров в корзине: {{ cart_total_price.total_price }} руб.</h5>
+      </div>
+    </template>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: "CartPage"
+  data() {
+    return {
+      cart_products: [],
+      cart_total_price: '',
+      }
+    },
+
+  mounted() {
+
+    //    сохранение в переменной токена авторизации полученного из localStorage
+    const token = localStorage.getItem('AUTH_TOKEN')
+    //     отправка гет запроса в заголовке которого токен авторизации
+//          получение товаров в корзине
+    this.$axios.get('/api/v1/cart-products/', {
+      headers: {
+        Authorization: "Token " + token
+      }
+    }).then(response => {
+
+      console.log(response)
+//          добавление в cart_products списка товаров корзины полученного из response data
+      this.cart_products = response.data
+      console.log(this.cart_products)
+    }),
+
+//          получение общей цены товаров в корзине
+      this.$axios.get('/api/v1/cart-products/cart-total-price/', {
+        headers: {
+          Authorization: "Token " + token
+        }
+      }).then(response => {
+
+        console.log(response)
+//          добавление в cart_products списка товаров корзины полученного из response data
+        this.cart_total_price = response.data
+        console.log(this.cart_total_price)
+      })
+}
 }
 </script>
 
