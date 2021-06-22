aa637286771de45f0add9f0d9decf7990c6d6c7a
Arif_Gafizov <agafizov@gmail.com>
2021-06-22 23:03:18 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -25,6 +25,8 @@ export default function (/* { store, ssrContext } */) {
     mode: process.env.VUE_ROUTER_MODE,
     base: process.env.VUE_ROUTER_BASE
   })
-
+  // Router.beforeEach((to, from, next) => {
+  //   // ...store
+  // })
   return Router
 }
