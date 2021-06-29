c38306b87b45246ce9cd7bba9879b07a00f74f23
Arif_Gafizov <agafizov@gmail.com>
2021-06-29 23:09:00 +0300

    Add AddProductPage.vue

---
+++ src/pages/AddProductPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page>
+    <q-toolbar-title>Add products</q-toolbar-title>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "AddProductPage"
+}
+</script>
+
+<style scoped>
+
+</style>
